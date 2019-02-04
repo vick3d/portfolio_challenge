b931c172035e50e4c3675caff6394809211c176a
vick3d <viktoro0090@gmail.com>
2019-02-04 15:47:18 +0100

    install axios and fetch 'Project data' from data file instead of hardcoded

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import axios from "axios"
 
 
 
@@ -6,17 +7,17 @@ class Projects extends Component {
     constructor() {
         super();
         this.state = {
-            projects: [
-                {
-                    "id": 1,
-                    "name": "My first Website"
-                },
-                {
-                    "id": 2,
-                    "name": "FizzBuzz - Ruby"
-                }
-            ]
+            projects: []
         };
+    }
+
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+            .then(response => {
+                this.setState({
+                    projects: response.data
+                })
+            })
     }
 
     render() {
