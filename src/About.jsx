9951fe835d0e5c89d5fa3ca8efa860035695e649
vick3d <viktoro0090@gmail.com>
2019-02-04 22:14:07 +0100

    install Undraw-illustrations add images to About and My Projects

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,23 @@
 import React from "react"
+import { UndrawDesignerLife } from 'react-undraw-illustrations';
 
 
 const About = () => {
     return (
-        <div>
-            <h1 className="content-text">About Me</h1>
-            <p className="content-text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Mollitia consequuntur maiores explicabo dolorem adipisci iure quaerat ullam incidunt perferendis, 
-                asperiores aperiam accusamus voluptates ex commodi nesciunt quis doloremque doloribus ipsa?
-            </p>
+        <div className="content-wrapper">
+            <div className="flex flex-wrap mb-4">
+                <div className="w-full lg:w-1/4 md:w-1/2">
+                    <UndrawDesignerLife primaryColor='#12283a' height='200px' />
+                </div>
+                <div className="w-full lg:w-3/4 md:w-1/2">
+                    <h1 className="content-text">About me</h1>
+                <p className="content-text"> 
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Mollitia consequuntur maiores explicabo dolorem adipisci iure quaerat ullam incidunt perferendis, 
+                    asperiores aperiam accusamus voluptates ex commodi nesciunt quis doloremque doloribus ipsa?
+                </p>
+                </div>
+            </div>
         </div>
     )
 };
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react"
 import axios from "axios"
 import ProjectCard from "./ProjectCard"
+import { UndrawDashboard } from "react-undraw-illustrations";
+
 
 
 
@@ -37,9 +39,19 @@ class Projects extends Component {
         }
 
         return (
-            <div>
-                <h1 className="content-text">My Projects</h1>
+            <div className="content-wrapper">
+                <div className="flex mb-4">
+                    <div className="w-1/4">
+                        <UndrawDashboard primaryColor='#12283a' height='200px' />
+                    </div>
+                    <div className="w-3/4">
+                        <h1>My Projects</h1>
+                        <p>This is a selection of some of my projects I have been working on.</p>
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-1 lg:-mx-4">
                 {projectsList}
+                </div>
             </div>
         )
     }
