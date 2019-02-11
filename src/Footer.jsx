e6c7dd259e9910e0651589ce9de1e8c279211276
vick3d <viktoro0090@gmail.com>
2019-02-11 10:48:42 +0100

    add email Icon

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div className="footer-wrapper">
-            <div className="footer-content">
-                Made with <span className="lname">React</span>
-                <a href="https://github.com/vick3d"><img className="footer-image" src="./src/img/git-logo.png"></img></a>
-            </div>
+        <div className="footer">
+           <ul className="footer-ulist">
+                    <li><a href="mailto:viktoro0090@gmail.com"><i className="fa fa-envelope footer-envelope footer-list"></i></a></li>
+                    <li><a href="https://github.com/vick3d"><img className="footer-list" src="./src/img/git-logo.png"></img></a></li>
+            </ul>
         </div>
     )
 }
